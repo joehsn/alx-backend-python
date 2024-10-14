4ebc918129e0dd1b35f5f2e69389c758385b4875
joehsn <joehsn@outlook.com>
2024-10-14 21:41:41 +0000

    0. The basics of async task is formatted

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -9,9 +9,9 @@ import random
 
 
 async def wait_random(max_delay: int = 10) -> float:
-  '''
-    Waits for a random number of seconds.
-  '''
-  wait_time = random.random() * max_delay
-  await asyncio.sleep(wait_time)
-  return wait_time
+    '''
+        Waits for a random number of seconds.
+    '''
+    wait_time = random.random() * max_delay
+    await asyncio.sleep(wait_time)
+    return wait_time
